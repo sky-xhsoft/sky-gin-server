1137fe793a8afe03e727497f19bdd9f6ffffa366
skyzhou <77086539@163.com>
2025-01-18 23:25:03 +0800

    Add go mod file  and create   main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sky-xhsoft/sky-gin-server
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------
+// Project Name: sky-gin-server
+// File Name: main.go
+// Author: xhsoftware-skyzhou
+// Created On: 2025/1/18
+// Project Description:
+// ----------------------------------------------------------------------------
+
+package main
+
+func main() {
+
+}
